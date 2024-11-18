3dd8e229dc8421f23b713d25051ecec1bb60cea1
will-miles <willmiles@live.co.uk>
2024-11-18 14:07:51 -0700

    current page nav button now greyed out

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,19 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Nav from './Nav.js';
 import { Link } from 'react-router-dom';
 
-const Header = () => {
-  return (
-    <div className='flex border-b-2 border-alabaster'>
-      <div className='flex-1 m-4'>
-        <Link className='text-alabaster' to='/'>
-          <span className=''>Will Miles</span>
-          <span className='hidden sm:inline'> - Full-stack developer</span>
-        </Link>
+class Header extends Component {
+  state = { path: window.location.pathname };
+
+  render() {
+    return (
+      <div className='flex border-b-2 border-alabaster'>
+        <div className='flex-1 m-4'>
+          <Link className='text-alabaster' to='/'>
+            <span className=''>Will Miles</span>
+            <span className='hidden sm:inline'> - Full-stack developer</span>
+          </Link>
+        </div>
+        <Nav path={this.state.path} handler={this.handler} className='flex-1' />
       </div>
-      <Nav className='flex-1' />
-    </div>
-  );
-};
+    );
+  }
+
+  handler = (newPath) => {
+    this.setState({ path: newPath });
+    this.forceUpdate();
+  };
+}
 
 export default Header;
--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,44 +1,55 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 
-const Nav = () => {
-  console.log(window.location.pathname);
-  return (
-    <div>
-      <ul className='hidden sm:flex text-center min-w-block'>
-        {makeNavLinks(window.location.pathname)}
-      </ul>
-      <div className='sm:hidden text-alabaster m-4'>
-        <FontAwesomeIcon icon={faBars} />
+class Nav extends Component {
+  render() {
+    return (
+      <div>
+        <div className='hidden sm:flex text-center min-w-block'>
+          {this.makeNavLinks()}
+        </div>
+        <div className='sm:hidden text-alabaster m-4'>
+          <FontAwesomeIcon icon={faBars} />
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
 
-const makeNavLinks = (pathname) => {
-  const NavArr = [
-    { path: '/', text: 'Home' },
-    { path: '/projects', text: 'Projects' },
-    { path: '/contact', text: 'Contact' },
-  ];
+  makeNavLinks = () => {
+    const NavArr = [
+      { path: '/', text: 'Home' },
+      { path: '/projects', text: 'Projects' },
+      { path: '/contact', text: 'Contact' },
+    ];
 
-  return NavArr.map((navItem) =>
-    pathname === navItem.path ? (
-      <Link key={navItem.text.toLowerCase()} to={navItem.path}>
-        <li className='pr-3 pt-1 pl-3 pb-1 rounded-full bg-alabaster m-3 mr-1.5 ml-1.5 opacity-70'>
+    return NavArr.map((navItem) =>
+      this.props.path === navItem.path ? (
+        <Link
+          onClick={() => {
+            this.props.handler(navItem.path);
+          }}
+          className='pr-3 pt-1 pl-3 pb-1 rounded-full bg-alabaster m-3 mr-1.5 ml-1.5 opacity-70'
+          key={navItem.text.toLowerCase()}
+          to={navItem.path}
+        >
           {navItem.text}
-        </li>
-      </Link>
-    ) : (
-      <Link key={navItem.text.toLowerCase()} to={navItem.path}>
-        <li className='pr-3 pt-1 pl-3 pb-1 rounded-full bg-alabaster m-3 mr-1.5 ml-1.5 hover:opacity-70'>
+        </Link>
+      ) : (
+        <Link
+          onClick={() => {
+            this.props.handler(navItem.path);
+          }}
+          className='pr-3 pt-1 pl-3 pb-1 rounded-full bg-alabaster m-3 mr-1.5 ml-1.5 hover:opacity-70'
+          key={navItem.text.toLowerCase()}
+          to={navItem.path}
+        >
           {navItem.text}
-        </li>
-      </Link>
-    )
-  );
-};
+        </Link>
+      )
+    );
+  };
+}
 
 export default Nav;
